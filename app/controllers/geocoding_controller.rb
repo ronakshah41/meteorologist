292031af3c03e62920bd4374b0044a3c6ed2bf96
Ronak Shah <ronakshah41@users.noreply.github.com>
2017-04-18 13:57:44 -0500

    Code for homework

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -15,11 +15,13 @@ class GeocodingController < ApplicationController
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
 
+    url_address = @street_address.gsub(" ", "+")
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + url_address
+    parsed_data = JSON.parse(open(url).read)
 
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
 
-    @latitude = "Replace this string with your answer."
-
-    @longitude = "Replace this string with your answer."
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("geocoding/street_to_coords.html.erb")
   end
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -14,18 +14,28 @@ class MeteorologistController < ApplicationController
     #
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
+    url_address = @street_address.gsub(" ", "+")
+    url_geo = "http://maps.googleapis.com/maps/api/geocode/json?address=" + url_address
+    parsed_data_geo = JSON.parse(open(url_geo).read)
+
+    @lat = parsed_data_geo["results"][0]["geometry"]["location"]["lat"]
+
+    @lng = parsed_data_geo["results"][0]["geometry"]["location"]["lng"]
+
+    url_forecast = "https://api.darksky.net/forecast/b41db97f786238109133d67a29d3251a/" + @lat.to_s + ","+ @lng.to_s
+    parsed_data_forecast = JSON.parse(open(url_forecast).read)
 
 
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = parsed_data_forecast["currently"]["temperature"]
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = parsed_data_forecast["currently"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data_forecast["minutely"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data_forecast["hourly"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_data_forecast["daily"]["summary"]
 
     render("meteorologist/street_to_weather.html.erb")
   end
